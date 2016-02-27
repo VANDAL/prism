VANDAL/prism

commit d282b2daff18de7a8b7323141217a516c380b3a7

Sigrind event generation start is now user defined
A command line option to Valgrind/Sigrind allows users to specify
which function to begin generating events at; event generation ends
when this function exists. The name is not demangled.

src/Sigil2/FrontEnds/Sigrind/valgrind-3.11.0-Sigil2/sigrind/threads.c
@@ -132,12 +132,12 @@ thread_info* new_thread(void)
 void SGL_(switch_thread)(ThreadId tid)
 {
   if (tid == SGL_(active_tid))
-  {
     return;
-  }
 
   SGL_(active_tid) = tid;
-  SGL_(log_sync)(SGLPRIM_SYNC_SWAP, SGL_(active_tid));
+
+  if ( EVENT_GENERATION_ENABLED )
+    SGL_(log_sync)(SGLPRIM_SYNC_SWAP, SGL_(active_tid));
 }
 
 void CLG_(switch_thread)(ThreadId tid)

src/Sigil2/FrontEnds/Sigrind/valgrind-3.11.0-Sigil2/sigrind/callstack.c
@@ -28,6 +28,7 @@
 
 #include "global.h"
 #include "log_events.h"
+#include "Sigil2/PrimitiveEnums.h"
 
 /*------------------------------------------------------------*/
 /*--- Call stack, operations                               ---*/
@@ -109,12 +110,15 @@ static void function_entered(fn_node* fn)
 {
   CLG_ASSERT(fn != 0);
 
-  if ( VG_(strcmp)(fn->name, "main") == 0 )
+  if ( (SGL_(clo).collect_func != NULL) && (VG_(strcmp)(fn->name, SGL_(clo).collect_func) == 0) )
   {
-	  VG_(umsg)("******************************************\n");
-	  VG_(umsg)("Entering main: turning on event collection\n");
-	  VG_(umsg)("******************************************\n");
-	  is_in_main = 1;
+    VG_(umsg)("*********************************************\n");
+    VG_(umsg)("Entering %s: turning on event collection\n", fn->name);
+    VG_(umsg)("*********************************************\n");
+    SGL_(is_in_event_collect_func) = True;
+
+    // let Sigil2 know which thread this function starts in
+    SGL_(log_sync)(SGLPRIM_SYNC_SWAP, SGL_(active_tid));
   }
   /* send to sigil */
   SGL_(log_fn_entry)(fn);
@@ -125,23 +129,23 @@ static void function_entered(fn_node* fn)
     CLG_(clo).verbose = fn->verbosity;
     fn->verbosity = old;
     VG_(message)(Vg_DebugMsg, 
-		 "Entering %s: Verbosity set to %d\n",
-		 fn->name, CLG_(clo).verbose);
+    "Entering %s: Verbosity set to %d\n",
+    fn->name, CLG_(clo).verbose);
   }
-#endif		
-}	
+#endif
+}
 
 /* Called when function left (no recursive level active) */
 static void function_left(fn_node* fn)
 {
   CLG_ASSERT(fn != 0);
 
-  if ( VG_(strcmp)(fn->name, "main") == 0 )
+  if ( (SGL_(clo).collect_func != NULL) && (VG_(strcmp)(fn->name, SGL_(clo).collect_func) == 0) )
   {
-	  VG_(umsg)("******************************************\n");
-	  VG_(umsg)("Leaving main: turning off event collection\n");
-	  VG_(umsg)("******************************************\n");
-	  is_in_main = 0;
+    VG_(umsg)("*********************************************\n");
+    VG_(umsg)("Leaving %s: turning off event collection\n", fn->name);
+    VG_(umsg)("*********************************************\n");
+    SGL_(is_in_event_collect_func) = False;
   }
   /*send to sigil*/
   SGL_(log_fn_leave)(fn);
@@ -152,10 +156,10 @@ static void function_left(fn_node* fn)
     CLG_(clo).verbose = fn->verbosity;
     fn->verbosity = old;
     VG_(message)(Vg_DebugMsg, 
-		 "Leaving %s: Verbosity set back to %d\n",
-		 fn->name, CLG_(clo).verbose);
+    "Leaving %s: Verbosity set back to %d\n",
+    fn->name, CLG_(clo).verbose);
   }
-#endif		
+#endif
 }
 
 

src/Sigil2/FrontEnds/Sigrind/valgrind-3.11.0-Sigil2/sigrind/clo.c
@@ -410,13 +410,14 @@ Bool CLG_(process_cmd_line_option)(const HChar* arg)
 {
    const HChar* tmp_str;
 
-   if      VG_STR_CLO(arg, "--tmp-dir", tmp_str) {
-      Int len = VG_(strlen)(tmp_str)+1;
-      SGL_(clo).tmp_dir = VG_(malloc)("sgl.clo.defaults", len*sizeof(*(SGL_(clo).tmp_dir)));
-      VG_(strncpy)(SGL_(clo).tmp_dir, tmp_str, len);
-      SGL_(clo).tmp_dir_len = len;
-   }
+   /* XXX tmpdir should not be set by the end-user, only for Sigil2 use */
+   if      VG_STR_CLO(arg, "--tmp-dir", SGL_(clo).tmpdir) {}
+   else if VG_STR_CLO(arg, "--at-func", SGL_(clo).collect_func) {}
 
+   /* XXX
+    * ML: leftover from Callgrind. Most of these should be left at defaults
+    * for Sigrind, except perhaps --separate callers depending on the application
+    */
    else if VG_BOOL_CLO(arg, "--skip-plt", CLG_(clo).skip_plt) {}
 
    else if VG_BOOL_CLO(arg, "--collect-jumps", CLG_(clo).collect_jumps) {}
@@ -617,7 +618,8 @@ void CLG_(print_debug_usage)(void)
 
 void SGL_(set_clo_defaults)(void)
 {
-  SGL_(clo).tmp_dir_len = 0;
+  SGL_(clo).tmpdir       = NULL;
+  SGL_(clo).collect_func = NULL;
 }
 
 void CLG_(set_clo_defaults)(void)

src/Sigil2/FrontEnds/Sigrind/valgrind-3.11.0-Sigil2/sigrind/global.h
@@ -75,9 +75,8 @@
 
 typedef struct _SglCommandLineOptions SglCommandLineOptions;
 struct _SglCommandLineOptions {
-   /* directory that will hold mmap'd file */
-   Int tmp_dir_len;
-   HChar* tmp_dir;
+  const HChar* tmpdir;
+  const HChar* collect_func;
 };
 
 typedef struct _CommandLineOptions CommandLineOptions;
@@ -787,10 +786,13 @@ void CLG_(init_dumps)(void);
 /*--- Exported global variables                            ---*/
 /*------------------------------------------------------------*/
 
-extern Bool is_in_main;//TODO allow user to set starting function
+extern Bool SGL_(is_in_event_collect_func); //ML: long name? feel free to change
 extern SglCommandLineOptions SGL_(clo);
 extern Bool* SGL_(thread_in_synccall);
 extern ThreadId SGL_(active_tid);
+
+#define EVENT_GENERATION_ENABLED  \
+   !SGL_(thread_in_synccall)[SGL_(active_tid)] && ( (SGL_(clo).collect_func == NULL) || SGL_(is_in_event_collect_func) )
 
 extern CommandLineOptions CLG_(clo);
 extern Statistics CLG_(stat);

src/Sigil2/FrontEnds/Sigrind/valgrind-3.11.0-Sigil2/sigrind/log_events.c
@@ -57,27 +57,29 @@ static UInt curr_used;
 static inline void incr_used(void);
 static inline void update_curr_buf(void);
 
-void SGL_(init_IPC)(const HChar *tmp_dir, Int len)
+void SGL_(init_IPC)()
 {
-	if (len < 2)
+	if (SGL_(clo).tmpdir == NULL)
 	{
 	   VG_(fmsg)("No --tmp-dir argument found, shutting down...\n");
 	   VG_(exit)(1);
 	}
 
+	Int tmpdir_len = VG_(strlen)(SGL_(clo).tmpdir);
 	Int filename_len;
+
 	//+1 for '/'; len should be strlen + null
-	filename_len = len + VG_(strlen)(SIGRIND_SHMEM_NAME) + 1; 
-	HChar *shmem_path = VG_(malloc)("sgl.open_shmem",filename_len*sizeof(*shmem_path));
-	VG_(snprintf)(shmem_path, filename_len, "%s/%s", tmp_dir, SIGRIND_SHMEM_NAME); 
+	filename_len = tmpdir_len + VG_(strlen)(SIGRIND_SHMEM_NAME) + 2; 
+	HChar shmem_path[filename_len];
+	VG_(snprintf)(shmem_path, filename_len, "%s/%s", SGL_(clo).tmpdir, SIGRIND_SHMEM_NAME); 
 
-	filename_len = len + VG_(strlen)(SIGRIND_EMPTYFIFO_NAME) + 1; 
-	HChar *emptyfifo_path = VG_(malloc)("sgl.open_shmem",filename_len*sizeof(*emptyfifo_path));
-	VG_(snprintf)(emptyfifo_path, filename_len, "%s/%s", tmp_dir, SIGRIND_EMPTYFIFO_NAME); 
+	filename_len = tmpdir_len + VG_(strlen)(SIGRIND_EMPTYFIFO_NAME) + 2; 
+	HChar emptyfifo_path[filename_len];
+	VG_(snprintf)(emptyfifo_path, filename_len, "%s/%s", SGL_(clo).tmpdir, SIGRIND_EMPTYFIFO_NAME); 
 
-	filename_len = len + VG_(strlen)(SIGRIND_FULLFIFO_NAME) + 1; 
-	HChar *fullfifo_path = VG_(malloc)("sgl.open_shmem",filename_len*sizeof(*fullfifo_path));
-	VG_(snprintf)(fullfifo_path, filename_len, "%s/%s", tmp_dir, SIGRIND_FULLFIFO_NAME); 
+	filename_len = tmpdir_len + VG_(strlen)(SIGRIND_FULLFIFO_NAME) + 2; 
+	HChar fullfifo_path[filename_len];
+	VG_(snprintf)(fullfifo_path, filename_len, "%s/%s", SGL_(clo).tmpdir, SIGRIND_FULLFIFO_NAME); 
 
 	///////////////////
 	// init values 

src/Sigil2/FrontEnds/Sigrind/valgrind-3.11.0-Sigil2/sigrind/log_events.h
@@ -17,7 +17,7 @@
  * Initializes interprocess communication with Sigil2.
  * THIS MUST BE RUN BEFORE ANY LOGGING FUNCTIONS ARE INVOKED 
  */
-void SGL_(init_IPC)(const HChar *tmp_dir, Int len);
+void SGL_(init_IPC)(void);
 void SGL_(finish_IPC)(void);
 
 /* 1 Instruction */

src/Sigil2/FrontEnds/Sigrind/valgrind-3.11.0-Sigil2/sigrind/sg_main.c
@@ -165,6 +165,7 @@ typedef struct {
 /*--- Global variables                                     ---*/
 /*------------------------------------------------------------*/
 
+Bool SGL_(is_in_event_collect_func);
 SglCommandLineOptions SGL_(clo);
 
 /* for all threads */
@@ -179,6 +180,7 @@ exec_state CLG_(current_state);
 /*------------------------------------------------------------*/
 /*--- Local declarations                                   ---*/
 /*------------------------------------------------------------*/
+
 static void flushEvents ( ClgState* clgs );
 static void addEvent_Comp( ClgState* clgs, InstrInfo* inode, IRExprTag arity, IRType op_type );
 static void addEvent_G ( ClgState* clgs, InstrInfo* inode );
@@ -235,12 +237,6 @@ static void addBBSetupCall(ClgState* clgs)
 			      argv);
    addStmtToIRSB( clgs->sbOut, IRStmt_Dirty(di) );
 }
-
-
-//FIXME this is hardcoded in for testing, to ignore all events
-//befor 'main'. This functionality should be given as an option
-//to the user, along with the exact name of the function.
-Bool is_in_main = 0; 
 
 static IRSB* CLG_(instrument)( VgCallbackClosure* closure,
                         IRSB* sbIn,
@@ -329,7 +325,7 @@ static IRSB* CLG_(instrument)( VgCallbackClosure* closure,
       }
 
       /* Ignore all Sigil event processing when inside a synchronization syscall */
-      if ( (SGL_(thread_in_synccall)[CLG_(current_tid)] == False) && is_in_main)
+      if ( EVENT_GENERATION_ENABLED )
       {
          switch (st->tag) 
          {
@@ -449,46 +445,43 @@ static IRSB* CLG_(instrument)( VgCallbackClosure* closure,
                addEvent_G(  &clgs, curr_inode );
                break;
             }
-
-            case Ist_LLSC: 
-            {
-               IRType dataTy;
-               if (st->Ist.LLSC.storedata == NULL) {
-                  /* LL */
-                  dataTy = typeOfIRTemp(sbIn->tyenv, st->Ist.LLSC.result);
-                  addEvent_Dr( &clgs, curr_inode,
-                               sizeofIRType(dataTy), st->Ist.LLSC.addr );
-                  /* flush events before LL, should help SC to succeed */
-                  flushEvents( &clgs );
-               } else {
-                  /* SC */
-                  dataTy = typeOfIRExpr(sbIn->tyenv, st->Ist.LLSC.storedata);
-                  addEvent_Dw( &clgs, curr_inode,
-                               sizeofIRType(dataTy), st->Ist.LLSC.addr );
-                  /* I don't know whether the global-bus-lock cost should
-                     be attributed to the LL or the SC, but it doesn't
-                     really matter since they always have to be used in
-                     pairs anyway.  Hence put it (quite arbitrarily) on
-                     the SC. */
-                  addEvent_G(  &clgs, curr_inode );
-               }
-               break;
-            }
             default:
                tl_assert(0);
                break;
          } //end switch
-      } //end !is_in_syscall
+      }
 
 
       /* 
-       * Still do jump/branch tracking regardless
-       * of whether or not we're in a sync syscall.
+       * Still do regardless of whether or not we're in a sync syscall.
        *
        * ML: not sure what side effects what be introduced
-       * by ignoring Exit statements, even in a syscall 
+       * by ignoring Exit statements, even in a syscall...let's avoid that
        */
-      if ( st->tag == Ist_Exit )
+      if ( st->tag == Ist_LLSC )
+      {
+         if (st->Ist.LLSC.storedata == NULL) {
+            /* LL */
+            if ( EVENT_GENERATION_ENABLED )
+            {
+               IRType dataTy = typeOfIRTemp(sbIn->tyenv, st->Ist.LLSC.result);
+               addEvent_Dr( &clgs, curr_inode, sizeofIRType(dataTy), st->Ist.LLSC.addr );
+            }
+            /* flush events before LL, should help SC to succeed */
+            flushEvents( &clgs );
+         } else if ( EVENT_GENERATION_ENABLED ) {
+            /* SC */
+            IRType dataTy = typeOfIRExpr(sbIn->tyenv, st->Ist.LLSC.storedata);
+            addEvent_Dw( &clgs, curr_inode, sizeofIRType(dataTy), st->Ist.LLSC.addr );
+            /* I don't know whether the global-bus-lock cost should
+               be attributed to the LL or the SC, but it doesn't
+               really matter since they always have to be used in
+               pairs anyway.  Hence put it (quite arbitrarily) on
+               the SC. */
+            addEvent_G(  &clgs, curr_inode );
+         }
+      }
+      else if ( st->tag == Ist_Exit )
       {
          Bool guest_exit, inverted;
 
@@ -545,7 +538,7 @@ static IRSB* CLG_(instrument)( VgCallbackClosure* closure,
                                  : IRExpr_RdTmp(guardW)
                                  ));
              /* And post the event. */
-             if ( (SGL_(thread_in_synccall)[CLG_(current_tid)] == False) && is_in_main )
+             if ( EVENT_GENERATION_ENABLED )
                addEvent_Bc( &clgs, curr_inode, IRExpr_RdTmp(guard) );
          }
 
@@ -607,7 +600,7 @@ static IRSB* CLG_(instrument)( VgCallbackClosure* closure,
          case Iex_Const:
             break; /* boring - branch to known address */
          case Iex_RdTmp:
-            if ( (SGL_(thread_in_synccall)[CLG_(current_tid)] == False) && is_in_main )
+            if ( EVENT_GENERATION_ENABLED )
                /* looks like an indirect branch (branch to unknown) */
                addEvent_Bi( &clgs, curr_inode, sbIn->next );
             break;
@@ -1383,27 +1376,27 @@ Bool CLG_(handle_client_request)(ThreadId tid, UWord *args, UWord *ret)
 	* is appropriate */
 										 
    case VG_USERREQ__SIGIL_PTHREAD_CREATE_ENTER:
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = True;
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = True;
       break;
    case VG_USERREQ__SIGIL_PTHREAD_CREATE_LEAVE:
-      /* log once the thread has been CREATED and waiting */
-      if ( is_in_main == 1 )
+      /* enable and log once the thread has been CREATED and waiting */
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = False;
+      if ( EVENT_GENERATION_ENABLED )
       {
          SGL_(log_sync)((UChar)SGLPRIM_SYNC_CREATE, args[1]);
       }
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = False;
       break;
 
    case VG_USERREQ__SIGIL_PTHREAD_JOIN_ENTER:
-      /* log when the thread join is ENTERED */
-      if ( is_in_main == 1 )
+      /* log when the thread join is ENTERED and disable */
+      if ( EVENT_GENERATION_ENABLED )
       {
          SGL_(log_sync)((UChar)SGLPRIM_SYNC_JOIN, args[1]);
       }
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = True;
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = True;
       break;
    case VG_USERREQ__SIGIL_PTHREAD_JOIN_LEAVE:
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = False;
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = False;
       break;
 
    case VG_USERREQ__SIGIL_GOMP_LOCK_ENTER:
@@ -1412,7 +1405,7 @@ Bool CLG_(handle_client_request)(ThreadId tid, UWord *args, UWord *ret)
    case VG_USERREQ__SIGIL_GOMP_CRITNAMESTART_ENTER:
    case VG_USERREQ__SIGIL_GOMP_ATOMICSTART_ENTER:
    case VG_USERREQ__SIGIL_PTHREAD_LOCK_ENTER:
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = True;
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = True;
       break;
    case VG_USERREQ__SIGIL_GOMP_SETLOCK_LEAVE:
    case VG_USERREQ__SIGIL_GOMP_LOCK_LEAVE:
@@ -1420,12 +1413,12 @@ Bool CLG_(handle_client_request)(ThreadId tid, UWord *args, UWord *ret)
    case VG_USERREQ__SIGIL_GOMP_CRITNAMESTART_LEAVE:
    case VG_USERREQ__SIGIL_GOMP_ATOMICSTART_LEAVE:
    case VG_USERREQ__SIGIL_PTHREAD_LOCK_LEAVE:
-      /* log once the lock has been acquired */
-      if ( is_in_main == 1 )
+      /* enable and log once the lock has been acquired */
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = False;
+      if ( EVENT_GENERATION_ENABLED )
       {
          SGL_(log_sync)((UChar)SGLPRIM_SYNC_LOCK, args[1]);
       }
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = False;
       break;
 
    case VG_USERREQ__SIGIL_GOMP_UNLOCK_ENTER:
@@ -1434,7 +1427,7 @@ Bool CLG_(handle_client_request)(ThreadId tid, UWord *args, UWord *ret)
    case VG_USERREQ__SIGIL_GOMP_CRITNAMEEND_ENTER:
    case VG_USERREQ__SIGIL_GOMP_ATOMICEND_ENTER:
    case VG_USERREQ__SIGIL_PTHREAD_UNLOCK_ENTER:
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = True;
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = True;
       break;
    case VG_USERREQ__SIGIL_GOMP_UNLOCK_LEAVE:
    case VG_USERREQ__SIGIL_GOMP_UNSETLOCK_LEAVE:
@@ -1442,78 +1435,79 @@ Bool CLG_(handle_client_request)(ThreadId tid, UWord *args, UWord *ret)
    case VG_USERREQ__SIGIL_GOMP_CRITNAMEEND_LEAVE:
    case VG_USERREQ__SIGIL_GOMP_ATOMICEND_LEAVE:
    case VG_USERREQ__SIGIL_PTHREAD_UNLOCK_LEAVE:
-      if ( is_in_main == 1 )
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = False;
+      if ( EVENT_GENERATION_ENABLED )
       {
          SGL_(log_sync)((UChar)SGLPRIM_SYNC_UNLOCK, args[1]);
       }
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = False;
       break;
 
    case VG_USERREQ__SIGIL_GOMP_BARRIER_ENTER:
    case VG_USERREQ__SIGIL_GOMP_TEAMBARRIERWAIT_ENTER:
    case VG_USERREQ__SIGIL_GOMP_TEAMBARRIERWAITFINAL_ENTER:
    case VG_USERREQ__SIGIL_PTHREAD_BARRIER_ENTER:
-      /* log once the barrier is ENTERED and waiting */
-      if ( is_in_main == 1 )
+      /* log once the barrier is ENTERED and waiting and disable */
+      if ( EVENT_GENERATION_ENABLED )
       {
          SGL_(log_sync)((UChar)SGLPRIM_SYNC_BARRIER, args[1]);
       }
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = True;
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = True;
       break;
    case VG_USERREQ__SIGIL_GOMP_BARRIER_LEAVE:
    case VG_USERREQ__SIGIL_GOMP_TEAMBARRIERWAIT_LEAVE:
    case VG_USERREQ__SIGIL_GOMP_TEAMBARRIERWAITFINAL_LEAVE:
    case VG_USERREQ__SIGIL_PTHREAD_BARRIER_LEAVE:
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = False;
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = False;
       break;
 
    case VG_USERREQ__SIGIL_PTHREAD_CONDWAIT_ENTER:
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = True;
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = True;
       break;
    case VG_USERREQ__SIGIL_PTHREAD_CONDWAIT_LEAVE:
-      if ( is_in_main == 1 )
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = False;
+      if ( EVENT_GENERATION_ENABLED )
       {
          SGL_(log_sync)((UChar)SGLPRIM_SYNC_CONDWAIT, args[1]);
       }
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = False;
       break;
 
    case VG_USERREQ__SIGIL_PTHREAD_CONDSIG_ENTER:
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = True;
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = True;
       break;
    case VG_USERREQ__SIGIL_PTHREAD_CONDSIG_LEAVE:
-      if ( is_in_main == 1 )
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = False;
+      if ( EVENT_GENERATION_ENABLED )
       {
          SGL_(log_sync)((UChar)SGLPRIM_SYNC_CONDSIG, args[1]);
       }
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = False;
       break;
 
    case VG_USERREQ__SIGIL_PTHREAD_SPINLOCK_ENTER:
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = True;
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = True;
       break;
    case VG_USERREQ__SIGIL_PTHREAD_SPINLOCK_LEAVE:
-      if ( is_in_main == 1 )
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = False;
+      if ( EVENT_GENERATION_ENABLED )
       {
          SGL_(log_sync)((UChar)SGLPRIM_SYNC_SPINLOCK, args[1]);
       }
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = False;
       break;
 
    case VG_USERREQ__SIGIL_PTHREAD_SPINUNLOCK_ENTER:
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = True;
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = True;
       break;
    case VG_USERREQ__SIGIL_PTHREAD_SPINUNLOCK_LEAVE:
-      if ( is_in_main == 1 )
+      SGL_(thread_in_synccall)[SGL_(active_tid)] = False;
+      if ( EVENT_GENERATION_ENABLED )
       {
          SGL_(log_sync)((UChar)SGLPRIM_SYNC_SPINUNLOCK, args[1]);
       }
-      SGL_(thread_in_synccall)[CLG_(current_tid)] = False;
       break;
 
    default:
       return False;
    }
+
 
    return True;
 }
@@ -1574,8 +1568,16 @@ static void clg_start_client_code_callback ( ThreadId tid, ULong blocks_done )
 static
 void CLG_(post_clo_init)(void)
 {
-   // initialize interface to Sigil
-   SGL_(init_IPC)(SGL_(clo).tmp_dir, SGL_(clo).tmp_dir_len);
+   SGL_(init_IPC)(); // initialize interface to Sigil
+   SGL_(is_in_event_collect_func) = False;
+
+   if (SGL_(clo).collect_func == NULL)
+   {
+      VG_(umsg)("*********************************************\n");
+      VG_(umsg)("Beginning event generation from program start\n");
+      VG_(umsg)("*********************************************\n");
+      SGL_(is_in_event_collect_func) = True;
+   }
 
    if (VG_(clo_vex_control).iropt_register_updates_default
        != VexRegUpdSpAtMemAccess) {

commit 6a00e812069eabb3ce080958a604da33f546d47b

Fixed bug that caused sigil2 backend to think it was in the wrong thread
Bug introduced in commit: d282b2daff18de7a8b7323141217a516c380b3a7
Sigrind will block all event generation when inside a sync call or not
in a user defined function that triggers event generation. This also
disabled generating thread context switches, which could happen before
exiting a pthread join, for example. This change unblocks all thread
switch event generation so that the backend is always up-to-date on
the thread context.

src/Sigil2/FrontEnds/Sigrind/valgrind-3.11.0-Sigil2/sigrind/threads.c
@@ -136,8 +136,10 @@ void SGL_(switch_thread)(ThreadId tid)
 
   SGL_(active_tid) = tid;
 
-  if ( EVENT_GENERATION_ENABLED )
-    SGL_(log_sync)(SGLPRIM_SYNC_SWAP, SGL_(active_tid));
+  /* ML: always send thread switch events; 
+   * valgrind can change at any time, even if sigrind is 'inside' 
+   * a synchronization call or outside the function being collected */ 
+  SGL_(log_sync)(SGLPRIM_SYNC_SWAP, SGL_(active_tid));
 }
 
 void CLG_(switch_thread)(ThreadId tid)